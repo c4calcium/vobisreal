4e6299304d683d689f69a557b9140ab59a9cd7f6
cal <163433870+c4calcium@users.noreply.github.com>
2024-04-26 08:34:04 -0500

    vers5
    worked on attacking

--- Assets/scripts/ball.cs
+++ Assets/scripts/ball.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ball : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private float direction;
     private bool hit;
     private CircleCollider2D ocollider;
     private Animator anim;
@@ -16,13 +17,33 @@ public class ball : MonoBehaviour
     private void Update()
     {
         if (hit) return;
-        float movementspeed = speed * Time.deltaTime;
+        float movementspeed = speed * Time.deltaTime * direction;
         transform.Translate(movementspeed, 0, 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         hit = true;
-        //8:11
+        ocollider.enabled = false;
+        anim.SetTrigger("explode");
+    }
+
+    public void setdirection(float _direction)
+    {
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        ocollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if(Mathf.Sign(localScaleX) != _direction)
+            localScaleX = -localScaleX;
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+    
+    private void deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
