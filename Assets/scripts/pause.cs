ae65c8ecb695b4de5ebc22546620b81684aa1446
cal <163433870+c4calcium@users.noreply.github.com>
2024-05-06 08:33:27 -0500

    vers19
    started a pause menu

---
+++ Assets/scripts/pause.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+    private bool paused;
+    [SerializeField] private GameObject pausemenu;
+    private float waiting;
+
+    void Awake()
+    {
+        paused = false;
+        waiting = 0;
+    }
+
+
+    void Update()
+    {
+        if (waiting > 0)
+            waiting -= Time.deltaTime;
+
+        if (Input.GetKey("escape") || Input.GetKey(KeyCode.E))
+        {
+            if (paused == false && waiting <= 0)
+                {
+                    paused = true;
+                    pausemenu.SetActive(true);
+                    waiting = 0.5f;
+                }
+            if (paused == true && waiting <= 0)
+                {
+                    paused = false;
+                    pausemenu.SetActive(false);
+                    waiting = 0.5f;
+                }
+        }
+
+    
+    }
+    
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+}
